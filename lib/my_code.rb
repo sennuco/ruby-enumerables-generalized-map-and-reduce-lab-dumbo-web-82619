ae860f46f229d5088df0b2979d24e43f82c9b0da
Theresa Cyrus <cyrustheresa@gmail.com>
2019-08-06 15:12:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+#my own map
+
+def map(array) 
+ i=0
+ new_array =[]
+ while i< array.length do
+   new_array << yield(array[i])
+   i +=1
+ end
+ new_array
+end
+
+
+#my reduce
+def reduce(array, starting_point =nil)
+  i=0
+  total=starting_point
+  if starting_point
+    
+    
+  end
+  while i < array.length do
+    total = yield(array[i], total)
+  end
+   total
+end
